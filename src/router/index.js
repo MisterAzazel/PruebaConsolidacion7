985247ea2a63d676303a46f2c9d04eb3c19db684
Josué Peñaloza <josue20650@gmail.com>
2024-10-16 20:53:42 -0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdministracionView from '@/views/AdministracionView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/administracion',
+    name: 'administracion',
+    component: AdministracionView
   },
   {
     path: '/about',
